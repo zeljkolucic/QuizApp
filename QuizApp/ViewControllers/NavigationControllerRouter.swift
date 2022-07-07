605f42e37a4da3059d575a90447351b9860b6f1e
Zeljko Lucic <zeljko.lucic@levi9.com>
2022-07-07 23:25:21 +0200

    Implement NavigationControllerRouter

--- QuizApp/ViewControllers/NavigationControllerRouter.swift
+++ QuizApp/ViewControllers/NavigationControllerRouter.swift
@@ -8,16 +8,23 @@
 import UIKit
 import QuizEngine
 
+protocol ViewControllerFactory {
+    func questionViewController(for question: String, answerCallback: @escaping (String) -> Void) -> UIViewController
+}
+
 class NavigationControllerRouter: Router {
     
     let navigationController: UINavigationController
+    let factory: ViewControllerFactory
     
-    init(_ navigationController: UINavigationController) {
+    init(_ navigationController: UINavigationController, factory: ViewControllerFactory) {
         self.navigationController = navigationController
+        self.factory = factory
     }
     
     func routeTo(question: String, answerCallback: @escaping (String) -> Void) {
-        navigationController.pushViewController(UIViewController(), animated: true)
+        let viewController = factory.questionViewController(for: question, answerCallback: answerCallback)
+        navigationController.pushViewController(viewController, animated: true)
     }
     
     func routeTo(result: Result<String, String>) {
